443ef78f8fad58b47622bbb53448d75683d0fdcc
Promberger David <David.PROMBERGER@lbs4.salzburg.at>
2022-05-12 12:39:39 +0200

    erster Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        Scanner Einlesen = new Scanner(System.in);
+
+        System.out.println("Hallo Welt");
+
+        int i = Einlesen.nextInt();
+    }
+}
